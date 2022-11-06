16d1b459e5ee83a5822a6f4bba10d5345cf159cb
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-11-06 20:23:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,8 +54,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
-WEB_LINK = eenviron.get("WEB_LINK", "")
-API_KEY = environ.get("API_KEY", "")
+WEB_LINK = eenviron.get("WEB_LINK", "https://tnlink.in/api")
+API_KEY = environ.get("API_KEY", "b9558942eb7ba391657e1542a8e050340090bdd5")
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
