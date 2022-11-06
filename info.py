0c1be368f2c4542d3b6ed05cf8eabf3f8f6e6eba
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-11-06 19:58:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,6 +54,9 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+WEB_LINK = eenviron.get("WEB_LINK", "")
+API_KEY = environ.get("API_KEY", "")
+
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
