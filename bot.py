230cfc777f5addf1ba05376b46037e209d49f35a
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-11-23 20:49:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,12 @@ from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 
+from aiohttp import web
+from plugins.web_support import web_server
+
+
+PORT = "8080"
+
 class Bot(Client):
 
     def __init__(self):
@@ -40,6 +46,10 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
 
