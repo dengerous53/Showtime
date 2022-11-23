32cf08317dfcd96e26be202b4168055a351295ae
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-11-23 09:38:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import os
 from datetime import datetime
 from typing import List
 from database.users_chats_db import db
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 import aiohttp
 
