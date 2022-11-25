ff2f8a064dfe4672dc1aa6ed06a5851e41daf8d6
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-11-25 21:12:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get.get('SESSION', 'Media_search')
+SESSION = "Media_search"
 API_ID = int(environ.get('API_ID', '13985209'))
 API_HASH = environ.get('API_HASH', '9ecc14b98a6c7bfbc445f3b425a56e21')
 BOT_TOKEN = environ['BOT_TOKEN']
