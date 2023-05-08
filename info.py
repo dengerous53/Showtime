764c92cdd15b39fd77204822fcaaed14adacbf24
Nitishsehrawat <98394162+Nitishsehrawat@users.noreply.github.com>
2023-05-08 07:40:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,11 +49,11 @@ FILE_STORE_CHANNEL = None
 MELCOW_NEW_USERS = is_enabled(("True"), True)
 PROTECT_CONTENT = is_enabled(("False"), False)
 PUBLIC_FILE_STORE = is_enabled(("True"), True)
-WEB_LINK = "https://tnlink.in/api"
-API_KEY = "b9558942eb7ba391657e1542a8e050340090bdd5"
+WEB_LINK = "https://urlshortx.com/api"
+API_KEY = "e2408befbe4703f317696529571ec03adeed31ef"
 
 # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 200))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 1000))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
